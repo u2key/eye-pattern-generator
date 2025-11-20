ac6f3a3f4e0182ea727585250489d4d9e4b64fa6
u2key <248891400+u2key@users.noreply.github.com>
2025-11-20 18:51:55 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+#! /usr/bin/python3
+
+import csv
+import sys
+import numpy as np
+import matplotlib
+matplotlib.interactive(False)
+import matplotlib.pyplot as plt
+from copy import deepcopy
+from load-data import *
+from generate-simple-eye-pattern import *
+
+def main(alpha=1.0):
+  if len(sys.argv) < 3:
+    exit(1)
+  
+  csv_path = sys.argv[1]
+  csv_name = csv_path.replace(".csv", "")
+  print(f"CSV Name: {csv_name}")
+  bit_rate_mbps = np.float64(sys.argv[2])
+  bit_rate_bps = bit_rate_nbps * 1.0e+6
+  print(f"Bit Rate: {bit_rate_mbps} Mbps")
+  window_interval_ns = 1.0 / bit_rate_bps * 1.0e+9
+  print(f"Window Interval: {window_interval_ns} ns")
+  
+  (sample_interval, data) = load_data(csv_path)
+  samples_per_window = window_interval / sample_interval
+  print(f"Samples/Window = {samples_per_window} [samples]")
+  
+  generate_simple_eye_pattern(csv_name, bit_rate_mbps, samples_per_window, sample_interval, data, alpha)
+  
