7e01b15f9affdfebeb040f72689324c27c7f9eca
u2key <248891400+u2key@users.noreply.github.com>
2025-11-20 21:57:35 +0900

    Update generate_continuous_bits_eye_pattern.py

--- generate_continuous_bits_eye_pattern.py
+++ generate_continuous_bits_eye_pattern.py
@@ -9,7 +9,7 @@ def generate_continuous_bits_eye_pattern(csv_name, bit_rate_mbps, samples_per_wi
   maximum_number_of_continuous_bits = len(continuous_bits_data)
   samples_per_windows = [(samples_per_window * (a + 2.0)) for a in range(maximum_number_of_continuous_bits)]
   for a in range(maximum_number_of_continuous_bits):
-    number_of_continuous_bits = np.float(a + 1)
+    number_of_continuous_bits = np.float64(a + 1)
     time_ns = np.arange(samples_per_windows[a]) * sample_interval_ns
     if number_of_continuous_bits == 1:
       gauge_time_ns = np.array([samples_per_window * number_of_continuous_bits, samples_per_window * (number_of_continuous_bits + 0.5)])
