d990a51517fc80f5901b0087f5d1885e9489fd75
u2key <248891400+u2key@users.noreply.github.com>
2025-11-20 19:05:29 +0900

    Update generate_simple_eye_pattern.py

--- generate_simple_eye_pattern.py
+++ generate_simple_eye_pattern.py
@@ -11,7 +11,7 @@ def generate_simple_eye_pattern(csv_name, bit_rate_mbps, samples_per_window, sam
   print(f"###############################")
   bit_middle_ns = samples_per_window / 2 * sample_interval_ns
   print(f"Bit Middle: {bit_middle_ns} ns")
-  x = np.arange(int(samples_per_window)) * sample_interval_ns
+  time_ns = np.arange(int(samples_per_window)) * sample_interval_ns
   for channel in range(len(data[0])):
     plt.figure()
     axes = plt.axes()
@@ -23,9 +23,9 @@ def generate_simple_eye_pattern(csv_name, bit_rate_mbps, samples_per_window, sam
       if samples_end_index >= len(data):
         break
       samples = data[samples_start_index:samples_end_index, channel].flatten()
-      plt.plot(x, samples, color="green", alpha=alpha)
+      plt.plot(time_ns, samples, color="green", alpha=alpha)
       window_number = window_number + 1
-    plt.plot([bit_middle, bit_middle], [-1.0, 4.0], color="red")
+    plt.plot([bit_middle_ns, bit_middle_ns], [-1.0, 4.0], color="red")
     plt.gca().yaxis.set_major_formatter(plt.FormatStrFormatter('%.1f'))
     plt.ylim(-1.0, 4.0)
     plt.yticks([0.0, 3.3])
